e5f140bde922ec1abdb6b1e45ceef4c9a751ffd4
Sean McCulloch <86432671+seanmcculloch@users.noreply.github.com>
2026-01-07 10:26:06 -0800

    fill out example

--- src/aind_metadata_queries/examples/format_query_template.py
+++ src/aind_metadata_queries/examples/format_query_template.py
@@ -3,25 +3,22 @@
 # Import the necessary query from the registry, or the entire registry
 from aind_metadata_queries.queries import QUERIES_REGISTRY
 
-
-# Function to pretty-print a dictionary
-def pretty_print_dict(d):
-    import json
-
-    print(json.dumps(d, indent=2))
-
-
 # Main function to demonstrate formatting
 def run():
     # Retrieve the query template from the registry
-    query_template = QUERIES_REGISTRY["all_records_from_subject_id"]
+    wrapped_query = QUERIES_REGISTRY["all_records_from_subject_id"]
 
     # Define parameters to format the query template (name must match QueryParameter names)
-    my_parameters = {"subject_id": "my_subject_123"}
-    formatted = query_template.format(my_parameters)
+    my_parameters_dict = {"subject_id": "123456"}
+    formatted_nosql_query = wrapped_query.format(my_parameters)
 
-    # pretty print dict with 2 spaces indent
-    pretty_print_dict(formatted)
+    print("Final query", formatted_nosql_query)
+
+    # HERE use aind-data-access-api to query metadata DocDB.
+    records = [] # response will be a list of dicts
+    processed_records = wrapped_query.post_processing_function(records)
+
+    # HERE use processed records
 
 
 if __name__ == "__main__":
